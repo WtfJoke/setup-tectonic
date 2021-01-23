e694994a4479ca878c283c04831d49d23689cdf2
Manuel <wtfjoke@livenet.ch>
2021-01-23 01:16:35 +0100

    :sparkles: Extract tectonic release informations

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const RELEASE_TAG_IDENTIFIER = 'tectonic@'
+export const REPO_OWNER = 'tectonic-typesetting'
+export const REPO_NAME = 'tectonic'
---
+++ src/githubTypes.ts
@@ -0,0 +1,5 @@
+import {Endpoints} from '@octokit/types'
+import {GitHub} from '@actions/github/lib/utils'
+
+export type GithubReleaseAssets = Endpoints['GET /repos/{owner}/{repo}/releases/{release_id}']['response']['data']['assets']
+export type GithubOktokit = InstanceType<typeof GitHub>
