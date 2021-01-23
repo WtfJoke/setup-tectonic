18ac45528205ff920ff3e7c5198a23669b33e47a
Manuel <wtfjoke@livenet.ch>
2021-01-23 14:42:41 +0100

    :sparkles: Add support for linux/mac :apple::penguin:

--- src/setup-tectonic.ts
+++ src/setup-tectonic.ts
@@ -1,5 +1,9 @@
-import * as core from '@actions/core'
 import * as os from 'os'
+import * as fs from 'fs'
+import * as path from 'path'
+import {v4 as uuid} from 'uuid'
+import * as io from '@actions/io'
+import * as core from '@actions/core'
 import * as tc from '@actions/tool-cache'
 import {getTectonicRelease, Release} from './release'
 
@@ -14,22 +18,49 @@ function mapOS(osKey: string): string {
 
 async function downloadTectonic(url: string): Promise<string> {
   core.debug(`Downloading Tectonic from ${url}`)
-  const zipPath = await tc.downloadTool(url)
+  const archivePath = await tc.downloadTool(url)
 
-  core.debug('Extracting Tectonic zip file')
-  const tectonicPath = await tc.extractZip(zipPath)
-  core.debug(`Tectonic path is ${tectonicPath}.`)
+  core.debug('Extracting Tectonic')
+  let tectonicPath = ''
+  if (url.endsWith('.zip')) {
+    tectonicPath = await tc.extractZip(archivePath)
+  } else if (url.endsWith('.tar.gz')) {
+    tectonicPath = await tc.extractTar(archivePath)
+  } else if (url.endsWith('.AppImage')) {
+    tectonicPath = await createPathForAppImage(archivePath)
+  }
 
-  if (!zipPath || !tectonicPath) {
+  core.debug(`Tectonic path is ${tectonicPath}`)
+
+  if (!archivePath || !tectonicPath) {
     throw new Error(`Unable to download tectonic from ${url}`)
   }
 
   return tectonicPath
 }
 
+async function createPathForAppImage(appPath: string): Promise<string> {
+  const tectonicPath = await createTempFolder(appPath)
+  const newAppPath = path.resolve(tectonicPath, 'tectonic')
+  await io.mv(appPath, newAppPath)
+
+  core.debug(`Moved Tectonic from ${appPath} to ${newAppPath}`)
+
+  // make it executable
+  fs.chmodSync(newAppPath, '755')
+
+  return tectonicPath
+}
+
+async function createTempFolder(pathToExecutable: string): Promise<string> {
+  const destFolder = path.join(path.dirname(pathToExecutable), uuid())
+  await io.mkdirP(destFolder)
+  return destFolder
+}
+
 export async function setUpTectonic(): Promise<Release> {
   try {
-    const githubToken = core.getInput('github-token')
+    const githubToken = core.getInput('github-token', {required: true})
     const version = core.getInput('tectonic_version')
     // const useBiber = core.getInput('setup_biber') === 'true'
 
