c3192951f59d0aa696e193699d7ec947b3dd3dff
Thanh <quang.thanh.ta@pr1meti.de>
2022-02-08 18:29:26 +0100

    Correctly handle Biber versioning (#147)
    * test(biber): Expand tests on biber versions
    
    * fix(biber): Correctly handle Biber versioning
    
    * test(biber): Add tests for validBiberVersion

--- __tests__/biber.test.ts
+++ __tests__/biber.test.ts
@@ -1,7 +1,8 @@
-import {buildDownloadURL, downloadBiber} from '../src/biber'
+import {buildDownloadURL, downloadBiber, validBiberVersion} from '../src/biber'
 import * as fs from 'fs'
 import * as path from 'path'
 import * as os from 'os'
+import {execFileSync} from 'child_process'
 
 const tempDir = path.join(__dirname, 'runner', 'temp')
 process.env['RUNNER_TEMP'] = tempDir
@@ -14,6 +15,31 @@ test('build download link', async () => {
   )
 })
 
+test('get biber version from invalid input', () => {
+  expect(validBiberVersion('')).toBe('current')
+  expect(validBiberVersion('invalid')).toBe('current')
+  expect(validBiberVersion('current')).toBe('current')
+  expect(validBiberVersion('; sudo shutdown -h now #')).toBe('current')
+  expect(validBiberVersion('" SELECT * FROM super_secret_db ;')).toBe('current')
+})
+
+test('get biber version as major.minor', () => {
+  expect(validBiberVersion('   1.0 ')).toBe('1.0')
+  expect(validBiberVersion('  0.6.0 ')).toBe('0.6')
+  expect(validBiberVersion(' v 0.0.0 ')).toBe('0.0')
+})
+
+test('get biber version as major.minor.patch', () => {
+  expect(validBiberVersion('   0.9.8 ')).toBe('0.9.8')
+  expect(validBiberVersion('   0.9.9  ')).toBe('0.9.9')
+  expect(validBiberVersion('   0.9.10 ')).toBe('0.9.10')
+  expect(validBiberVersion(' 2.16.1')).toBe('2.16.1')
+})
+
+test('download non-existant biber version', async () => {
+  await expect(downloadBiber('0.0.0')).rejects.toThrow()
+}, 20000)
+
 test('download specific biber version', async () => {
   const biberPath = await downloadBiber('2.15')
   const fileExtension = os.platform() == 'win32' ? '.exe' : ''
@@ -21,6 +47,9 @@ test('download specific biber version', async () => {
 
   expect(fs.existsSync(biberPath)).toBe(true)
   expect(fs.existsSync(expectedBinaryPath)).toBe(true)
+  expect(execFileSync(expectedBinaryPath, ['--version']).toString()).toMatch(
+    /2\.15/
+  )
 }, 20000)
 
 test('download invalid biber version, should install current', async () => {
