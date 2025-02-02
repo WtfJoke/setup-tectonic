c5da4c072c2bdee1e7ca62614e5aebe65c165eb9
Manuel <wtfjoke@livenet.ch>
2025-02-02 19:21:03 +0100

    👷 Exclude tests in `lib` directory
    Tests are run after packaging and at this time there is already are compiled tests found in directory `lib` produced by npm run build.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,9 @@
 import {defineConfig} from 'vitest/config'
+import {configDefaults} from 'vitest/config'
 
 export default defineConfig({
   test: {
-    globals: true
+    globals: true,
+    exclude: [...configDefaults.exclude, 'lib']
   }
 })
